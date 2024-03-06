46f5b8bfaaaeb9d0dcd9e5339613e3d1996989eb
Badan Ser <157625611+Zeta-XD@users.noreply.github.com>
2024-03-07 00:21:00 +0530

    Update sudo.js

--- plugins/sudo.js
+++ plugins/sudo.js
@@ -1,14 +1,16 @@
 const { command, isPrivate } = require("../lib/");
-const config = require("../config");
+const X = require("../config");
 const { SUDO } = require("../config");
 command(
-  { pattern: "gsudo ?(.*)", 
+  { pattern: "getsudo ?(.*)", 
     fromMe: isPrivate, 
     desc: "shows sudo numbers", 
     type: "heroku" 
   },
   async (message, match, mm) => {
-    let zeta = config.SUDO
+    let zeta = X.SUDO
 message.reply("```" + `SUDO number are : ${zeta}` + "```")
   }
 );
+
+// Zeta-XD 😦
