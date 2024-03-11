69213bf1b66f851376fa699caed81eda459bb5b7
Badan Ser <157625611+Zeta-XD@users.noreply.github.com>
2024-03-11 10:41:04 +0530

    Update downloader.js

--- plugins/downloader.js
+++ plugins/downloader.js
@@ -13,19 +13,11 @@ command(
     },
     async (message, match) => {
         if (!match) return await message.sendMessage("*_Need Song Name Or Url_*");
-var res = await axios.get(`https://api-viper-x.koyeb.app/api/song?name=${match}`)
+var res = await axios.get(`https://api-viper-x.koyeb.app/api/song?name=amor+dues`)
 var song = res.data
 await message.client.sendMessage(message.jid, { text: `*_Downloading ${song.data.title}_*` },{ quoted: message})
-const aswinsparky = await (await fetch(`${song.data.downloadUrl}`)).buffer()
-await message.client.sendMessage(message.jid, { audio :aswinsparky,  mimetype:"audio/mpeg", contextInfo: { externalAdReply: {
-title: "𝐄𝐙𝐑𝐀-𝐗𝐃",
-body: "𝙎𝙤𝙣𝙜 𝘿𝙤𝙬𝙣𝙡𝙤𝙖𝙙𝙚𝙙 𝙎𝙪𝙘𝙘𝙚𝙨𝙨𝙛𝙪𝙡𝙡𝙮",
-sourceUrl: "",
-mediaUrl: "",
-mediaType: 1,
-showAdAttribution: true,
-renderLargerThumbnail: false,
-thumbnailUrl: "https://i.imgur.com/Ou56ggv.jpeg" }} }, {quoted: message })
+const zeta = await (await fetch(`${song.data.downloadUrl}`)).buffer()
+await message.client.sendMessage(message.jid, { audio :zeta,  mimetype:"audio/mpeg" }, {quoted: message })
     }
     );
 
