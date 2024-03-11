f18606cd3b006e19bf503516546a656ba1fb90d8
Badan Ser <157625611+Zeta-XD@users.noreply.github.com>
2024-03-11 10:21:07 +0530

    Create gpt.js

---
+++ plugins/gpt.js
@@ -0,0 +1,13 @@
+const { command, isPrivate, getJson } = require("../lib/");
+command(
+    {
+        pattern: "gpt",
+        fromMe: isPrivate,
+        desc: "chat gpt",
+        type: "user",
+    },
+    async (message, match) => {
+        if (!match) return await message.reply(`*_Need Text_*\n*Eg:- .gpt Hi*`);
+let {data} = await getJson(`https://api.vihangayt.asia/ai/chatgpt?q=${match}`);
+await message.sendMessage(message.jid, { text: data })
+});
