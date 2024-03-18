de7c018700aac702f80eb6034c1c7a8f1d5a9406
Badan Ser <157625611+Zeta-XD@users.noreply.github.com>
2024-03-18 11:39:17 +0530

    Update media.js

--- plugins/media.js
+++ plugins/media.js
@@ -72,17 +72,7 @@ async function gimage(query, amount = 5) {
   });
 }
 
-/* Copyright (C) 2022 X-Electra.
-Licensed under the  GPL-3.0 License;
-you may not use this file except in compliance with the License.
-X-Asena - X-Electra
-*/
 
-/* Copyright (C) 2022 X-Electra.
-Licensed under the  GPL-3.0 License;
-you may not use this file except in compliance with the License.
-X-Asena - X-Electra
-*/
 command(
   {
     pattern: "tts",
@@ -123,25 +113,9 @@ command(
 if (!message.reply_message || (!message.reply_message.video && !message.reply_message.image)) return await message.reply('*_Reply at image/video!_*')
 let res = await m.quoted.download();
       if(message.reply_message.video){
-       await message.client.sendMessage(message.jid, { video :res ,  mimetype:"video/mp4", contextInfo: { externalAdReply: {
-title: "𝐄𝐙𝐑𝐀-𝐗𝐃",
-body: "𝘾𝙖𝙥𝙩𝙞𝙤𝙣 𝘾𝙝𝙖𝙣𝙜𝙚𝙙",
-sourceUrl: "",
-mediaUrl: "",
-mediaType: 1,
-showAdAttribution: true,
-renderLargerThumbnail: false,
-thumbnailUrl: "https://i.imgur.com/Ou56ggv.jpeg" }}, caption: (match)}, {quoted: message })
+       await message.client.sendMessage(message.jid, { video :res ,  mimetype:"video/mp4", caption: (match)}, {quoted: message })
       } else if(message.reply_message.image){
-      await message.client.sendMessage(message.jid, { image :res ,  mimetype:"image/jpeg", contextInfo: { externalAdReply: {
-title: "𝐄𝐙𝐑𝐀-𝐗𝐃",
-body: "𝘾𝙖𝙥𝙩𝙞𝙤𝙣 𝘾𝙝𝙖𝙣𝙜𝙚𝙙",
-sourceUrl: "",
-mediaUrl: "",
-mediaType: 1,
-showAdAttribution: true,
-renderLargerThumbnail: false,
-thumbnailUrl: "https://i.imgur.com/Ou56ggv.jpeg" }}, caption: (match)}, {quoted: message })
+      await message.client.sendMessage(message.jid, { image :res ,  mimetype:"image/jpeg", caption: (match)}, {quoted: message })
 }
   }
   );
@@ -167,15 +141,7 @@ command(
     if (message.reply_message.mtype !== "stickerMessage")
       return await message.reply("*_Not a sticker_*");
     let buff = await m.quoted.download();
-    return await message.sendMessage(buff,{mimetype: 'image/jpeg', contextInfo: { externalAdReply: {
-title: "𝐄𝐙𝐑𝐀-𝐗𝐃",
-body: "𝘾𝙤𝙣𝙫𝙚𝙧𝙩𝙚𝙙 𝙏𝙤 𝙋𝙝𝙤𝙩𝙤",
-sourceUrl: "",
-mediaUrl: "",
-mediaType: 1,
-showAdAttribution: true,
-renderLargerThumbnail: false,
-thumbnailUrl: "https://i.imgur.com/Ou56ggv.jpeg" }}, caption: (config.CAPTION), quoted: message }, "image")
+    return await message.sendMessage(buff,{mimetype: 'image/jpeg', caption: (config.CAPTION), quoted: message }, "image")
   }
 );
 
@@ -199,15 +165,7 @@ command(
       return await message.reply("*_Not a sticker_*");
     let buff = await m.quoted.download();
     let buffer = await webp2mp4(buff);
-    return await message.sendMessage(buffer,{mimetype: 'video/mp4', contextInfo: { externalAdReply: {
-title: "𝐄𝐙𝐑𝐀-𝐗𝐃",
-body: "𝘾𝙤𝙣𝙫𝙚𝙧𝙩𝙚𝙙 𝙏𝙤 𝙈𝙥𝟰",
-sourceUrl: "",
-mediaUrl: "",
-mediaType: 1,
-showAdAttribution: true,
-renderLargerThumbnail: false,
-thumbnailUrl: "https://i.imgur.com/Ou56ggv.jpeg" }}, caption: (config.CAPTION), quoted: message }, "video")
+    return await message.sendMessage(buffer,{mimetype: 'video/mp4', caption: (config.CAPTION), quoted: message }, "video")
   }
 );
 
@@ -229,15 +187,7 @@ command(
     if (!message.reply_message || (!message.reply_message.video && !message.reply_message.audio)) return await message.reply('*_Reply at audio/voice/video!_*')  
     let buff = await m.quoted.download();
     buff = await toAudio(buff, "mp3");
-     await message.sendMessage(buff,{mimetype: 'audio/mpeg', contextInfo: { externalAdReply: {
-title: "𝐄𝐙𝐑𝐀-𝐗𝐃",
-body: "𝘾𝙤𝙣𝙫𝙚𝙧𝙩𝙚𝙙 𝙏𝙤 𝙈𝙥𝟯",
-sourceUrl: "",
-mediaUrl: "",
-mediaType: 1,
-showAdAttribution: true,
-renderLargerThumbnail: false,
-thumbnailUrl: "https://i.imgur.com/Ou56ggv.jpeg" }}, quoted: message }, "audio");
+     await message.sendMessage(buff,{mimetype: 'audio/mpeg', quoted: message }, "audio");
   }
 );
 
@@ -254,247 +204,10 @@ command(
     if (message.reply_message.mtype == "stickerMessage"){
       let buff = await m.quoted.download();
    let buffer = await webp2mp4(buff);
-   await message.client.sendMessage(message.jid, { video: { url : buffer }, gifPlayback: true, contextInfo: { externalAdReply: {
-title: "𝐄𝐙𝐑𝐀-𝐗𝐃",
-body: "𝘾𝙤𝙣𝙫𝙚𝙧𝙩𝙚𝙙 𝙏𝙤 𝙂𝙞𝙛",
-sourceUrl: "",
-mediaUrl: "",
-mediaType: 1,
-showAdAttribution: true,
-renderLargerThumbnail: false,
-thumbnailUrl: "https://i.imgur.com/Ou56ggv.jpeg" }}, caption: (config.CAPTION)},{ quoted: message });
+   await message.client.sendMessage(message.jid, { video: { url : buffer }, gifPlayback: true, caption: (config.CAPTION)},{ quoted: message });
    } else if(message.reply_message.video){
     let buff = await m.quoted.download();
-   await message.client.sendMessage(message.jid, { video: buff, gifPlayback: true, contextInfo: { externalAdReply: {
-title: "𝐄𝐙𝐑𝐀-𝐗𝐃",
-body: "𝘾𝙤𝙣𝙫𝙚𝙧𝙩𝙚𝙙 𝙏𝙤 𝙂𝙞𝙛",
-sourceUrl: "",
-mediaUrl: "",
-mediaType: 1,
-showAdAttribution: true,
-renderLargerThumbnail: false,
-thumbnailUrl: "https://i.imgur.com/Ou56ggv.jpeg" }}, caption: (config.CAPTION)},{ quoted: message });
+   await message.client.sendMessage(message.jid, { video: buff, gifPlayback: true, caption: (config.CAPTION)},{ quoted: message });
   }
   }
 );
-/*
-command(
-  {
-    pattern: "song",
-    fromMe: isPrivate,
-    desc: "Downloads Song",
-    type: "downloader",
-  },
-  async (message, match) => {
-    if (!(match || message.reply_message.text))
-      return await message.reply("_Enter Song Name_");
-    match = match || message.reply_message.text;
-    if (ytIdRegex.test(match)) {
-      yta(match.trim()).then(async ({ dl_link, title, thumb }) => {
-        message.reply("1 " + dl_link)
-      let buff = await AddMp3Meta(dl_link, thumb, {
-          title,
-        });
-        message.sendMessage(
-          buff,
-          { mimetype: "audio/mpeg", quoted: message.data },
-          "audio"
-        );
-      });
-    }
-    search(match + "song").then(async ({ videos }) => {
-      await message.reply(`_Downloading ${videos[0].title}_`);
-      let a = videos[0].url
-    //  let b = a.url
-      await message.reply(`${a}`)
-      yta(videos[0].url).then(async ({ dl_link, title, thumb }) => {
-   //     message.reply()
-        let buff = await AddMp3Meta(dl_link, thumb, {
-          title,
-          artist: [videos[0].author],
-        });
-        message.sendMessage(
-          buff,
-          { mimetype: "audio/mpeg", quoted: message.data },
-          "audio"
-        );
-      });
-    });
-  }
-);
-
-/* Copyright (C) 2022 X-Electra.
-Licensed under the  GPL-3.0 License;
-you may not use this file except in compliance with the License.
-X-Asena - X-Electra
-*/
-
-/*command(
-  {
-    pattern: "video ?(.*)",
-    fromMe: isPrivate,
-    desc: "Downloads video",
-    type: "downloader",
-  },
-  async (message, match) => {
-  //  if (!match || !message.reply_message.text)
-   //   return await message.reply("_Enter Video Name_");
-    match = match || message.reply_message.text;
-    if (ytIdRegex.test(match)) {
-      ytv(match.trim()).then(({ dl_link, title }) => {
-        message.sendFromUrl(dl_link, { filename: title });
-      });
-    }
-    search(match).then(async ({ videos }) => {
-      await message.reply(`_Downloading ${videos[0].title}_`);
-      ytv(videos[0].url).then(({ dl_link, title }) => {
-        message.sendFromUrl(dl_link, { filename: title, quoted: message });
-      });
-    });
-  }
-);*/
-
-
-
-/* Copyright (C) 2022 X-Electra.
-Licensed under the  GPL-3.0 License;
-you may not use this file except in compliance with the License.
-X-Asena - X-Electra
-*/
-
-/* Copyright (C) 2022 X-Electra.
-Licensed under the  GPL-3.0 License;
-you may not use this file except in compliance with the License.
-X-Asena - X-Electra
-*/
-
-/*command(
-  {
-    pattern: "fetch",
-    fromMe: isPrivate,
-    desc: "Downloads from a direct link",
-    type: "downloader",
-  },
-  async (message, match) => {
-    match = match || message.reply_message.text;
-    if (!match)
-      return message.reply(
-        "_Send a direct media link_\n_*link;caption(optional)*_"
-      );
-    try {
-      let url = match.split(";")[0];
-      let options = {};
-      options.caption = match.split(";")[1];
-
-      if (isUrl(url)) {
-        message.sendFromUrl(url, options);
-      } else {
-        message.reply("_Not a URL_");
-      }
-    } catch (e) {
-      console.log(e);
-      message.reply("_No content found_");
-    }
-  }
-);*/
-/* Copyright (C) 2022 X-Electra.
-Licensed under the  GPL-3.0 License;
-you may not use this file except in compliance with the License.
-X-Asena - X-Electra
-*/
-
-
-//message.reply_message.text
-
-/*command(
-  {
-    pattern: "yts",
-    fromMe: isPrivate,
-    desc: "Search Youtube",
-    type: "Search",
-  },
-  async (message, match) => {
-    if (!match) return await message.reply("_Enter a search term_");
-    let rows = [];
-    search(match).then(async ({ videos }) => {
-      videos.forEach((result) => {
-        rows.push({
-          title: result.title,
-          description: `\nDuration : ${result.duration.toString()}\nAuthor : ${
-            result.author
-          }\nPublished : ${result.ago}\nDescription : ${
-            result.description
-          }\nURL : ${result.url}`,
-          rowId: ` `,
-        });
-      });
-      await message.client.sendMessage(message.jid, {
-        text: "Youtube Search for " + match,
-        buttonText: "View Results",
-        sections: [
-          {
-            title: "Youtube Search",
-            rows: rows,
-          },
-        ],
-      });
-    });
-  }
-);*/
-
-/* Copyright (C) 2022 X-Electra.
-Licensed under the  GPL-3.0 License;
-you may not use this file except in compliance with the License.
-X-Asena - X-Electra
-*/
-
-/*command(
-  {
-    pattern: "ytv",
-    fromMe: isPrivate,
-    dontAddCommandList: true,
-  },
-  async (message, match) => {
-    match = match || message.reply_message.text;
-    if (!match) return await message.reply("_Enter a URL_");
-
-    if (!ytIdRegex.test(match)) return await message.reply("_Invalid Url_");
-    ytv(match).then(async ({ dl_link, title }) => {
-      await message.reply(`_Downloading ${title}_`);
-      return await message.sendFromUrl(dl_link, {
-        filename: title,
-        quoted: message,
-      });
-    });
-  }
-);
-
-/* Copyright (C) 2022 X-Electra.
-Licensed under the  GPL-3.0 License;
-you may not use this file except in compliance with the License.
-X-Asena - X-Electra
-*/
-
-/*command(
-  {
-    pattern: "yta",
-    fromMe: isPrivate,
-    dontAddCommandList: true,
-  },
-  async (message, match) => {
-    match = match || message.reply_message.text;
-    if (!match) return await message.reply("*_Enter a URL_*");
-    if (!ytIdRegex.test(match)) return await message.reply("*_Invalid Url_*");
-    yta(match).then(async ({ dl_link, title, thumb }) => {
-      await message.reply(`*_Downloading ${title}_*`);
-      let buff = await AddMp3Meta(dl_link, thumb, {
-        title,
-      });
-      return await message.sendMessage(
-        buff,
-        { mimetype: "audio/mpeg", quoted: message.data },
-        "audio"
-      );
-    });
-  }
-);*/
