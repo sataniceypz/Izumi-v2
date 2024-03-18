c91837b5aeab71a9336e3bc6225cc25fa9204513
Badan Ser <157625611+Zeta-XD@users.noreply.github.com>
2024-03-18 10:29:11 +0530

    Create 2gpt.js

---
+++ plugins/2gpt.js
@@ -0,0 +1,23 @@
+const { command, isPrivate, getJson } = require("../lib/");
+command(
+    {
+        pattern: "2gpt",
+        fromMe: isPrivate,
+        desc: "chat gpt",
+        type: "user",
+    },
+    async (message, match) => {
+        if (!match) return await message.reply(`*_Need Text_*\n*Eg:- .gpt Hi*`);
+let kko = await message.sendFromUrl("https://i.imgur.com/dHVUYg6.jpeg",
+      {caption: "*Thinking....👀*"}, {quoted: message})
+      let {result} = await getJson(`https://guruapi.tech/api/chatgpt?text=${match}`);
+await client.relayMessage(message.jid, {
+        protocolMessage: {
+          key: kko.key,
+          type: 14,
+          editedMessage: {
+            imageMessage: { caption: result }
+          }
+        }
+      }, {});
+});
