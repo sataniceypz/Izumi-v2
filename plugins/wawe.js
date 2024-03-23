ba9e555b5df69cdf3feb78ab27438a1d127fc63a
Badan Ser <157625611+Zeta-XD@users.noreply.github.com>
2024-03-24 00:00:12 +0530

    Update wawe.js

--- plugins/wawe.js
+++ plugins/wawe.js
@@ -12,14 +12,6 @@ command(
     let buff = await m.quoted.download();
         let media = await toPTT(buff);
 
-        return await message.sendMessage(media, { mimetype: 'audio/mpeg', ptt: true, contextInfo: { externalAdReply: {
-title: "𝐄𝐙𝐑𝐀-𝐗𝐃",
-body: "𝘾𝙤𝙣𝙫𝙚𝙧𝙩𝙚𝙙 𝙏𝙤 𝙑𝙤𝙞𝙘𝙚",
-sourceUrl: "",
-mediaUrl: "",
-mediaType: 1,
-showAdAttribution: true,
-renderLargerThumbnail: false,
-thumbnailUrl: "https://i.imgur.com/Ou56ggv.jpeg" }}, quoted: message }, "audio");
+        return await message.sendMessage(media, { mimetype: 'audio/mpeg', ptt: true }, quoted: message }, "audio");
   }
 );
