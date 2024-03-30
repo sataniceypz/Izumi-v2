e1a7eb8354669ef3af333ee22ba8ab68a11ca1e8
Badan Ser <157625611+Zeta-XD@users.noreply.github.com>
2024-03-30 22:19:34 +0530

    Update xvd.js

--- plugins/xvd.js
+++ plugins/xvd.js
@@ -13,14 +13,6 @@ command(
 var sex = await fetch(`https://raganork-network.vercel.app/api/xvideos/download?url=${match}`);
         var fek = await sex.json();
 
-        await message.client.sendMessage(message.jid,{video:{ url: fek.url}, contextInfo: { externalAdReply: {
-title: "𝐄𝐙𝐑𝐀-𝐗𝐃",
-body: "𝙑𝙞𝙙𝙚𝙤 𝘿𝙤𝙬𝙣𝙡𝙤𝙖𝙙𝙚𝙙 𝙎𝙪𝙘𝙘𝙚𝙨𝙨𝙛𝙪𝙡𝙡𝙮",
-sourceUrl: "",
-mediaUrl: "",
-mediaType: 1,
-showAdAttribution: true,
-renderLargerThumbnail: false,
-thumbnailUrl: "https://i.imgur.com/Ou56ggv.jpeg" }}, caption : (config.CAPTION)}, {quoted : message})
+        await message.client.sendMessage(message.jid,{video:{ url: fek.url}, caption : (config.CAPTION)}, {quoted : message})
     }
     );
