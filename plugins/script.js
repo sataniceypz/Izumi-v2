66c1c21cd192b520b4d7f4bc70d552e931c8557c
Eypz Satânico <105191630+sataniceypz@users.noreply.github.com>
2024-04-27 09:46:30 +0530

    script.js

--- plugins/script.js
+++ plugins/script.js
@@ -9,17 +9,16 @@ command(
     },
     async (message, match, m, client) => {
         let { data } = await axios.get('https://api.github.com/repos/Zeta-XD/REPO-TESTs')
-        let cap = `\n*𝚭͓ᴇ͛ᴛ͛ᴀ͛-𝚾͓ᴅ͛*🔋
+        let cap = `\n*𝐈𝐙𝐔𝐌𝐈-𝐗𝐃*🧚‍♂️
 
 *𝐒͓ɪ͛ᴍ͛ᴩ͛ʟ͛ᴇ᷍ 𝐖ʜ͛ᴀ͛ᴛ͛ꜱ͛ᴀ͛ᴩ͛ᴩ᷍ 𝚳͓ᴜ̟̽ʟ̟ᴛ̽ɪ͛ 𝐃̟͛ᴇ͛ᴠ͛ɪ͛ᴄ͛ᴇ͛ 𝚩͓ᴏ͛ᴛ͛ 𝚩̟ᴀ͛ꜱ͛ᴇ͛ᴅ͛ 𝚯̟ɴ̽ 𝚾̟ᴀ͛ꜱ̟͛ᴇ̟ɴ̟ᴀ͛*🖥️
 
-*𝚪⃯ᴇ͛ᴩ͛ᴏ͛ 𝐔͍ʀ͛ʟ͛* : ````https://github.com/Kingbadan321/Zeta-XD````
+*𝚪⃯ᴇ͛ᴩ͛ᴏ͛ 𝐔͍ʀ͛ʟ͛* : ````https://github.com/sataniceypz/Izumi-v2````
 
 ❒ *𝐅͍ᴏ͛ʀ͛ᴋ͛ 𝚫ɴ͛ᴅ͛ 𝐒͓̽ᴛ͛ᴀ̟ʀ̟*
 
-*𝐅ᴜ͛ʟ͛ʟ ͛𝚯ᴩ͛͢ᴇɴ͛ 𝐒ᴏ͛ᴜ͛ʀ͍͛ᴄ͛ᴇ͛*
 
-➫ *𝐒͢͢ᴇ͛ꜱ͛ꜱ͍͛ɪ͛ᴏ͛ɴ͛* : ````badan-ser.xyz````
+➫ *𝐒͢͢ᴇ͛ꜱ͛ꜱ͍͛ɪ͛ᴏ͛ɴ͛* : ````https://qr-izumi.onrender.com/````
 
 *𝚻⃯͓ʜ⃯͛ᴀ͛ɴ⃯͛ᴋ͛ꜱ͛*🦋
 
