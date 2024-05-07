bd99d3cfbc65c7a756564600e381e8a89862b37d
rikkubot <106902912+rikkubot@users.noreply.github.com>
2024-05-07 08:28:16 +0530

    Update support.js

--- plugins/support.js
+++ plugins/support.js
@@ -6,8 +6,8 @@ command({
     desc: "Astro Support",
     type: "support"
 }, async (message) => {
-    const name = 'ASTRO ☔', title = "Iᴢᴜᴍɪ Sᴜᴘᴘᴏʀᴛ🧚‍♂️", number = '919846272202', body = "ASTRO☔";
-    const image = "https://i.imgur.com/JS0TpvM.jpeg", sourceUrl = 'https://github.com/rikkubot/Izumi-v2';
+    const name = 'ASTRO ☔', title = "Asᴛʀᴏ Sᴜᴘᴘᴏʀᴛ🧚‍♂️", number = '919846272202', body = "ASTRO☔";
+    const image = "https://i.imgur.com/wmWj0BX.jpeg", sourceUrl = 'https://github.com/rikkubot/Izumi-v2';
     const logo = await getBuffer(image);
     const vcard = `BEGIN:VCARD\nVERSION:3.0\nFN:${name}\nORG: powered by Izumi;\nTEL;type=CELL;type=VOICE;waid=${number}:${number}\nEND:VCARD`;
     const adon = { title, body, thumbnail: logo, mediaType: 1, mediaUrl: sourceUrl, sourceUrl, showAdAttribution: true, renderLargerThumbnail: false };
