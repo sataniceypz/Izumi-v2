0b8661c2d3c5d993cfb2c2347da9481074bafb50
Eypz Satânico <105191630+sataniceypz@users.noreply.github.com>
2024-05-09 07:48:56 +0530

    Update dalle.js

--- plugins/dalle.js
+++ plugins/dalle.js
@@ -10,5 +10,5 @@ command(
     async (message, match, m) => {
 match = match || message.reply_message.text;
 	if(!match) return await message.reply(`*_Need A Text_*\n*eg:- .dalle a modified gtr in garage*`);
-await message.sendFromUrl(`https://cute-tan-gorilla-yoke.cyclic.app/imagine?text=${match}`, {caption: "*zetahh👀*"})
+await message.sendFromUrl(`https://cute-tan-gorilla-yoke.cyclic.app/imagine?text=${match}`, {caption: "*Izumi🧚*"})
 });
